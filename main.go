4738a2122ec178240e7ab8ec0bf9c19e52db2a02
matehaxor03 <matehaxor03>
2022-10-29 07:51:04 +1100

    initial commit helloworld

--- main.go
+++ main.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
-	
+import (
+	"net/http"
+	"fmt"
+	"strings"
+)
+
+func formatRequest(r *http.Request) string {
+	var request []string
+
+	url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
+	request = append(request, url)
+	request = append(request, fmt.Sprintf("Host: %v", r.Host))
+	for name, headers := range r.Header {
+		name = strings.ToLower(name)
+		for _, h := range headers {
+			request = append(request, fmt.Sprintf("%v: %v", name, h))
+		}
+	}
+
+	if r.Method == "POST" {
+		r.ParseForm()
+		request = append(request, "\n")
+		request = append(request, r.Form.Encode())
+	}
+
+	return strings.Join(request, "\n")
 }
+
+func ProcessRequest(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte(formatRequest(req)))
+}
+
+
+func main() {
+	http.HandleFunc("/", ProcessRequest)
+
+	err := http.ListenAndServeTLS(":443", "server.crt", "server.key", nil)
+	if err != nil {
+		fmt.Println("ListenAndServe: ", err)
+	}
+}
+
+
