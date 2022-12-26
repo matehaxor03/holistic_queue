b0926ac6dcfd9f0bae6d9bd73ca0e58cb070ad87
matehaxor03 <matehaxor03>
2022-12-26 17:27:09 +1100

    clone the values to avoid concurrent exception

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.19
 
 require (
 	github.com/matehaxor03/holistic_db_client v0.0.426
-	github.com/matehaxor03/holistic_json v0.0.10
+	github.com/matehaxor03/holistic_json v0.0.11
 	github.com/matehaxor03/holistic_thread_safe v0.0.1
 )
 
--- queue/QueueServer.go
+++ queue/QueueServer.go
@@ -320,12 +320,25 @@ func NewQueueServer(port string, server_crt_path string, server_key_path string,
 		}
 		
 		if queue_mode == "PushBack" {
+			cloned_request, cloned_request_errors := request.Clone()
+			if cloned_request_errors != nil {
+				process_request_errors = append(process_request_errors, cloned_request_errors...)
+			} else if common.IsNil(cloned_request) {
+				process_request_errors = append(process_request_errors, fmt.Errorf("cloned request is nil"))
+			}
+
+			if len(process_request_errors) > 0 {
+				http_extension.WriteResponse(w, *request, process_request_errors)
+				return
+			}
+
 			var wg sync.WaitGroup
-			if !request.IsBoolTrue("[async]") {
+			if !cloned_request.IsBoolTrue("[async]") {
 				wg.Add(1)
 				wait_groups[*trace_id] = &wg
 			}			
-			queue_obj.PushBack(request)
+			
+			queue_obj.PushBack(cloned_request)
 
 			wakeup_processor_errors := wakeup_processor(*queue, *trace_id)
 			if wakeup_processor_errors != nil {
@@ -337,7 +350,7 @@ func NewQueueServer(port string, server_crt_path string, server_key_path string,
 				return
 			}
 
-			if !request.IsBoolTrue("[async]") {
+			if !cloned_request.IsBoolTrue("[async]") {
 				result_ptr, found := result_groups[*trace_id]
 				if !found {
 					wg.Wait()
@@ -354,16 +367,43 @@ func NewQueueServer(port string, server_crt_path string, server_key_path string,
 				empty_map := json.Map{"[queue]":"empty", "[trace_id]":*trace_id, "[queue_mode]":queue_mode, "[async]":*async}
 				request = &empty_map
 			}
+
+			cloned_request, cloned_request_errors := request.Clone()
+			if cloned_request_errors != nil {
+				process_request_errors = append(process_request_errors, cloned_request_errors...)
+			} else if common.IsNil(cloned_request) {
+				process_request_errors = append(process_request_errors, fmt.Errorf("cloned request is nil"))
+			}
+
+			if len(process_request_errors) > 0 {
+				http_extension.WriteResponse(w, *request, process_request_errors)
+				return
+			}
+
+			request = cloned_request
 		} else if queue_mode == "complete" {
 			if !request.IsBoolTrue("[async]") {
+				
+				cloned_request, cloned_request_errors := request.Clone()
+				if cloned_request_errors != nil {
+					process_request_errors = append(process_request_errors, cloned_request_errors...)
+				} else if common.IsNil(cloned_request) {
+					process_request_errors = append(process_request_errors, fmt.Errorf("cloned request is nil"))
+				}
+
+				if len(process_request_errors) > 0 {
+					http_extension.WriteResponse(w, *request, process_request_errors)
+					return
+				}
+
 				fmt.Println(string(body_payload))
-				result_groups[*trace_id] = request
+				result_groups[*trace_id] = cloned_request
 				wait_group, wait_group_found := wait_groups[*trace_id]
 				if wait_group_found {
 					wait_group.Done()
 					delete(wait_groups, *trace_id)
 				}
-				//result = *json_payload
+				request = cloned_request
 				//todo set errors from payload
 			}
 		} else {
